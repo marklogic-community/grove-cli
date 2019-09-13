9758ca6f5953d7e8b21783e13303e62b57588332
Matt Pileggi <matt.pileggi@marklogic.com>
2019-09-13 14:37:35 -0400

    GH-9: print version from package.json

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 #!/usr/bin/env node
+const package_json = require('./package.json');
 var program = require('commander');
 
 program
-  .version('0.1.0')
+  .version(package_json.version)
   .command('new <appName>', 'Set up a React / Node application')
   .command('config', 'Configure an existing React / Node application')
   .command('demo <appName>', 'Set up and run a React / Node demo app')
